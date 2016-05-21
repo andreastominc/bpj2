9b2ced186cbdf522556493531096549beede56e1
andreastominc <andreas.tominc@edu.campus02.at>
2016-05-21 12:45:17 +0200

    test hibernate

---
+++ hibernate/src/Pers.java
@@ -0,0 +1,45 @@
+
+
+import java.io.Serializable;
+
+public class Pers implements Serializable{
+//implements Serializable für Multi-Threads
+	
+	private int id;
+	private String first;
+	private String last;
+	
+	public Pers()
+	{
+	}
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getFirst() {
+		return first;
+	}
+
+	public void setFirst(String first) {
+		this.first = first;
+	}
+
+	public String getLast() {
+		return last;
+	}
+
+	public void setLast(String last) {
+		this.last = last;
+	}
+	
+	
+}
---
+++ hibernate/src/PersonImpl.java
@@ -0,0 +1,29 @@
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class PersonImpl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Configuration cfg = new Configuration();
+		cfg.configure("connections.cfg.xml");
+		
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session s = sf.openSession();
+		Transaction tx = s.beginTransaction();
+		
+		Pers person = new Pers();
+		person.setId(2);
+		person.setFirst("Marge");
+		person.setLast("Johns");
+		
+		s.save(person);
+		s.flush();
+		tx.commit();
+	}
+
+}
