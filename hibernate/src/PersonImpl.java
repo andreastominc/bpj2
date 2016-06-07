256bf5bffbb6dea15769df755eb3c4ba62b4b386
Michael Hödl <michael.hoedl@edu.campus02.at>
2016-06-07 20:42:43 +0200

    Methode zum Abfragen von Personen hinzugefuegt

--- hibernate/src/PersonImpl.java
+++ hibernate/src/PersonImpl.java
@@ -1,5 +1,7 @@
 
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -19,11 +21,43 @@ public class PersonImpl {
 		Pers person = new Pers();
 		person.setId(3);
 		person.setFirst("Philipp");
-		person.setLast("Hierzer");
+		person.setLast("Hierzerer");
 		
 		s.save(person);
 		s.flush();
 		tx.commit();
+		
+		System.out.println("Test queryPerson:");
+		queryPerson(s);
+		
+		s.close();
 	}
+	
+
+	/**
+	 * Methode zum Abfragen und Ausgeben aller Personen.
+	 * Zur Info:
+	 * wenn man via Hibernate HQL Query auf die DB abfragen will, 
+	 * dann muss man den Namen vom Type in der Query angeben, also "Pers",
+	 * und NICHT den Namen der DB-Tabelle ("person"). Dieses Mapping ist eh in
+	 * der Datei "mapping.hbm.xml" definiert.
+	 * 
+	 * @param session
+	 */
+	private static void queryPerson(Session session) {
+		System.out.println("-------------------------");
+		List <Pers> list = (List<Pers>) session.createQuery("from Pers").list();
+        
+        java.util.Iterator<Pers> iter = list.iterator();
+        while (iter.hasNext()) {
+        	Pers person = iter.next();
+        	System.out.println("Person: \"" + person.getFirst() +
+                          "\", " + person.getLast() +
+                          "\", " + person.getId());
+        }
+        session.getTransaction().commit();
+        System.out.println("-------------------------");
+	}
+
 
 }
