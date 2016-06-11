787858f2133b0734ebe74135fb6be9002248c1ee
Michael Hödl <michael.hoedl@edu.campus02.at>
2016-06-11 22:33:26 +0200

    Code Formatierung angepasst

--- hibernate/src/benutzerverwaltung/BenWorker.java
+++ hibernate/src/benutzerverwaltung/BenWorker.java
@@ -7,12 +7,12 @@ import java.sql.PreparedStatement;
 public class BenWorker {
 	
 	public void insertNewUser(String txtusername, String txtpassword) throws Exception
-	{																				// ACHTUNG: hier ggf. auf Port 3306 anpassen bei euch !!!!
+	{	  // ACHTUNG: hier ggf. auf Port 3306 anpassen bei euch !!!!
 		Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3307/hibernate_test","hibernate_test","hibernate_test");
 		PreparedStatement stmt = (PreparedStatement) con.prepareStatement("INSERT INTO benutzer (benutzername, passwort) VALUES " +"('"+txtusername+"','"+txtpassword+"');");
 		stmt.executeUpdate();
 	}
 
 	// to do .... auf Hibernate umbauen.
-	
+		
 }
